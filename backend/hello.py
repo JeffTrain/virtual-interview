fe3ff50e0dec15e3deadd8e890e106a77f0ae84f
Jefåf Tian <jeff.tian@outlook.com>
2018-06-09 16:47:56 +0800

    File upload backend

--- backend/hello.py
+++ backend/hello.py
@@ -1,8 +1,49 @@
-﻿from flask import Flask, request
+﻿import os
+from flask import Flask, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = set(['webm', 'mp4', 'png'])
+UPLOAD_FOLDER = 'uploads'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
 def hello():
     return "Hello World"
+
+
+def allowed_files(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads', methods=['POST', 'GET'])
+def uploads():
+    if request.method == 'POST':
+        if 'video' not in request.files:
+            return redirect(request.url + '?err=NoVideoPart')
+
+        video_file = request.files['video']
+        if video_file.filename == '':
+            return redirect(request.url + '?err=NoSelectedFile')
+
+        if video_file and allowed_files(video_file.filename):
+            filename = secure_filename(video_file.filename)
+            video_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            location = url_for('uploaded_file', filename=filename)
+
+            return redirect(location)
+
+
+        else:
+            return redirect(request.url + '?err=FileTypeNotSupported')
+    else:
+        return "Error"
+
+
+@app.route('/uploaded_file/<filename>', methods=['GET'])
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
